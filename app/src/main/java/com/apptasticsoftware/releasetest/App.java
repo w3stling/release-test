5125ea9716d5192800f3044a91b3370e8b6ccdaf
w3stling <peter.westling@gmail.com>
2022-01-16 10:33:21 +0100

    Improved greeting

--- app/src/main/java/com/apptasticsoftware/releasetest/App.java
+++ app/src/main/java/com/apptasticsoftware/releasetest/App.java
@@ -5,7 +5,7 @@ package com.apptasticsoftware.releasetest;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World!!";
     }
 
     public static void main(String[] args) {
